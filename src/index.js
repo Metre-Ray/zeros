cedb22d25138cfcc5aa625a33fb16b079c07685e
Metre-Ray <dgary@tut.by>
2018-09-22 18:46:40 +0300

    modified index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var answer = 0,
+      num = 5;
+  while (num < number) {
+    answer += Math.floor(number/num);
+    num *= 5;
+  }
+  return answer;
 }
